4b7393b4f238281f3f01381b37b999190673d34a
Alberto Souza <alberto.souza.99@gmail.com>
2014-11-30 13:20:52 -0200

    adicinando o script de atualizar as tabelas

---
+++ bin/updateTables.js
@@ -0,0 +1,27 @@
+var Sails = require('sails');
+
+function init() {
+  Sails.load({
+    port: 1930,
+    hooks: {
+      grunt: false,
+      socket: false,
+      pubsub: false
+    },
+    models: {
+      migrate: 'alter'
+    },
+    environment: 'development'
+  }, doneAll);
+}
+
+function doneAll(err){
+  if ( err ) {
+    sails.log.error('Error on create stub data', err);
+  }
+  //sails.load();
+  // end / exit
+  return process.exit();
+}
+
+init();
