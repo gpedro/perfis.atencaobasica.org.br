1f135a2802785db59e4d3bc695fc11ec853ee465
Alberto Souza <alberto.souza.99@gmail.com>
2014-12-10 01:18:24 -0200

    adicionando a configuração padrão do oauth

---
+++ config/auth.js
@@ -0,0 +1,19 @@
+module.exports.auth = {
+  // in current we.js version OR is provider OR consumer
+  isProvider: true,
+  isConsumer: false,
+  enableLogin: true,
+
+  cookieDomain: '.cdp.dev',
+  cookieName: 'wetoken',
+  cookieMaxAge: 900000,
+  cookieSecure: false,
+
+  honeypot: {
+    // add a honeypot key to enable this feature
+    key: null,
+    maxThreatScore: 80,
+    // enable honeypot check in tests?
+    checkInTests: false
+  }
+}
