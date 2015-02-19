389117dd9b76b16ef08d7b5a69c474a3b6563881
Sávio Lucena <saviogl@gmail.com>
2015-02-19 16:52:45 -0300

    comitando checkAvailability

--- client/app/controllers/AuthRegisterController.js
+++ client/app/controllers/AuthRegisterController.js
@@ -2,6 +2,29 @@ App.AuthRegisterController.reopen({
   registerUrl: '/signup',
   user: {},
   isChecked: false,
+
+  usernameInvalid: true,
+
+  checkAvailabity: function (){
+    var self = this;
+    // if (!self.get('user.isDirty')) {
+    //   self.set('usernameInvalid', true);
+    //   return Ember.$('#usernameResponse').text('');
+    // }    
+    if(!/^[A-Za-z]{3,20}$/.test(self.get('user.username'))){
+      self.set('usernameInvalid', true);
+      return Ember.$('#usernameResponse').text('Nome de usuário inválido');
+    };
+    Ember.$.get('/api/v1/auth/check-username-availability?username=' + self.get('user.username'), function(resp){            
+      self.set('usernameInvalid', !resp.isAvaible);
+      if (resp.isAvaible) {
+        return Ember.$('#usernameResponse').text(self.get('inputHelpUsername'));
+      } else {
+        return Ember.$('#usernameResponse').text('Nome de usuário já existente');
+      }
+    });
+  }.observes('user.username'), 
+
   actions: {
     register: function() {
       NProgress.start();
