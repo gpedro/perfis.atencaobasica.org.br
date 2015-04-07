de9d8a75c68a55c226a192ba8ab5646dfd4d8a28
albertosouza <alberto.souza.99@gmail.com>
2015-04-07 09:56:14 -0300

    removendo o cache das apis para evitar problemas de erro de cache no IE

--- config/policies.js
+++ config/policies.js
@@ -11,13 +11,21 @@ function defaultPolicy(req, res, next) {
       if (req.options.action == 'find' || req.options.action == 'findOne') {
         if( req.param('email') || req.param('cpf')) {
           sails.log.warn('Only admins can search users by cpf | TODO change to check if user can update with acl:', req.param('email'), req.param('cpf'));
-          return res.forbidden();  
+          return res.forbidden();
         }
       }
     }
   }
 
   if (!req.options) return next();
+
+  if (res.header) {
+    // set header to never cache API responses for skip IE 11 cache bug
+    res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    res.header('Expires', '-1');
+    res.header('Pragma', 'no-cache');
+  }
+
   req._sails.acl.canPolicy(req, function(err, can) {
     if(err) return res.serverError(err);
     if(can === false) return res.forbidden();
